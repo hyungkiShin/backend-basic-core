3c9aea68e39935aecc16b7c0e2f7a3ff8ba65514
hyungkiShin <tkaqkeldk@nate.com>
2022-11-13 23:22:25 +0900

    [docs] Spring 이녀석을 시작함을 알립니다.

--- src/main/java/com/example/core/MemberApp.java
+++ src/main/java/com/example/core/MemberApp.java
@@ -3,15 +3,20 @@ package com.example.core;
 import com.example.core.member.Grade;
 import com.example.core.member.Member;
 import com.example.core.member.MemberService;
-import com.example.core.member.MemberServiceImpl;
+import com.example.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        final MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        final MemberService memberService = appConfig.memberService();
 //        final MemberServiceImpl memberService = new MemberServiceImpl();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         final Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
--- src/main/java/com/example/core/OrderApp.java
+++ src/main/java/com/example/core/OrderApp.java
@@ -5,16 +5,27 @@ import com.example.core.member.Member;
 import com.example.core.member.MemberService;
 import com.example.core.order.Order;
 import com.example.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 
-        final AppConfig appConfig = new AppConfig();
-        final MemberService memberService = appConfig.memberService();
-        final OrderService orderService = appConfig.orderService();
+//        final AppConfig appConfig = new AppConfig();
+//        (v1)
 //        MemberService memberService = new MemberServiceImpl();
 //        OrderService orderService = new OrderServiceImpl();
+
+//        (v2)
+//        final MemberService memberService = appConfig.memberService();
+//        final OrderService orderService = appConfig.orderService();
+
+//        (v3) [Spring]
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        final MemberService memberService = appContext.getBean("memberService", MemberService.class);
+        final OrderService orderService = appContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
